cc5e72b66df040a2432fba767d33ca0315c95f53
Roman Goralchuk <gromka.vladyko@gmail.com>
2021-10-03 13:50:32 +0300

    final

--- src/main/java/by/it/academy/Servlet2.java
+++ src/main/java/by/it/academy/Servlet2.java
@@ -10,6 +10,7 @@ import java.io.PrintWriter;
 
 public class Servlet2 extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
